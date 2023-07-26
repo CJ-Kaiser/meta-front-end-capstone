579da68775122f678db3fb0704a36fcfa5be4ac9
CJ-Kaiser <cj_kaiser@live.com>
2023-07-26 05:44:52 -0400

    Added images for highlight cards. scaled down restaurauntfood.png

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -1,3 +1,12 @@
+import bruschetta from '../images/bruschetta.png';
+
+const cardData = [
+    {
+        name: 'Greek Salad',
+        description: 'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary crutons.',
+
+    }
+];
 
 function Highlights() {
     return (
